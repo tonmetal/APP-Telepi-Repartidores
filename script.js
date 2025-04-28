13dd8432d181ad19f16582b7f240e51a90f8e7f9
Tonmetal <tonvildia@gmail.com>
2025-04-28 03:24:33 +0200

    adding javascript

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function horasTelepi(hours,deliverys,burguer){
-    return hours*7.67 + deliverys*0.48 + burguer*0.5
+function Telepi(horas,pedidos,bar){
+    return horas*7.67 + pedidos*0.48 + bar*0.5
+}
+
+function calcular(){
+
+    var bar = parseFloat(document.getElementById("Bar").value);
+    var pedidos = parseFloat(document.getElementById("Pedidos").value);
+    var horas = parseFloat(document.getElementById("Horas").value);
+
+    if (isNaN(horas) || isNaN(pedidos) || isNaN(bar)){
+        alert("Por favor, ingresa valores válidos para todos los campos.");
+        return;
+    }
+
+    var resultado = Telepi(horas,pedidos,bar);
+
+    document.getElementById("Resultado").textContent = resultado.toFixed(2) + "€"
+
 }
